fe54a622f9035efe4f30f720eca5e72f2a7d379b
anaushad246 <anaushad246@gmail.com>
2025-06-23 01:19:56 +0530

    fix: started as new

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,  // This will call your backend
+  baseURL: import.meta.env.VITE_API_URL + "/api",  // This will call your backend
   withCredentials: true,
 });
 
